2528135796eb529e00167f4239c128a22f089b48
AH-repo <adrianhoma02@gmail.com>
2025-12-03 19:58:55 +0100

    Refactorisation+sample presentation

--- presentation.py
+++ presentation.py
@@ -5,8 +5,7 @@ from sklearn.cluster import AffinityPropagation as SklearnAP
 from affinitypropagation import AffinityPropagation
 
 # Dataset
-X, _ = make_blobs(n_samples=100, centers=3)
-
+X, _ = make_blobs(n_samples=1000, centers=3)
 
 my_ap = AffinityPropagation(damping=0.7)
 my_labels = my_ap.fit_predict(X)
@@ -16,7 +15,8 @@ sk_ap = SklearnAP(damping=0.7)
 sk_labels = sk_ap.fit_predict(X)
 
 # Plot
-fig, (ax1, ax2) = plot.subplots(1, 2, figsize=(12, 5))
+fig, (ax1, ax2, ax3) = plot.subplots(1, 3, figsize=(18, 5))
+
 
 ax1.scatter(X[:, 0], X[:, 1], c=my_labels, s=80)
 ax1.scatter(X[my_ap.cluster_centers_indices_][:, 0],
@@ -24,11 +24,15 @@ ax1.scatter(X[my_ap.cluster_centers_indices_][:, 0],
             c='red', marker='*', s=400, edgecolors='black', linewidth=2)
 ax1.set_title(f'AH implementation ({len(np.unique(my_labels))} clusters)')
 
-ax2.scatter(X[:, 0], X[:, 1], c=sk_labels, s=80)
+
 ax2.scatter(X[sk_ap.cluster_centers_indices_][:, 0],
             X[sk_ap.cluster_centers_indices_][:, 1],
             c='red', marker='*', s=400, edgecolors='black', linewidth=2)
 ax2.set_title(f'Scikit-learn ({len(np.unique(sk_labels))} clusters)')
+
+
+ax3.scatter(X[:, 0], X[:, 1], c='gray', s=80, alpha=0.6)
+ax3.set_title('Raw data (unclustered)')
 
 plot.tight_layout()
 plot.show()
